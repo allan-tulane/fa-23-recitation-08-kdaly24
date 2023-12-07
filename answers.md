8a97ecaa0f055053554922b1885ae7e6360d6203
Killian_Daly <kdaly3@tulane.edu>
2023-12-06 22:20:06 -0600

    Final

--- answers.md
+++ answers.md
@@ -2,7 +2,7 @@
 
 ## Answers
 
-**Name:**_________________________
+**Name:**____Killian Daly______
 **Name:**_________________________
 
 
@@ -11,8 +11,8 @@ Place all written answers from `recitation-08.md` here for easier grading.
 
 
 - **1b)**
-
+This algorithm is designed to find the short path through the edges, while analyzing the vertices. With this in mind, $O(|V|log|E|)$
 
 
 - **2b)**
-
+This is a BFS model, or $O(|V| + |E|)$
--- main.py
+++ main.py
@@ -12,20 +12,54 @@ def shortest_shortest_path(graph, source):
       a dict where each key is a vertex and the value is a tuple of
       (shortest path weight, shortest path number of edges). See test case for example.
     """
-    ### TODO
-    pass
-    
+    def sspath(pastset, frontier):
+        
+        if len(frontier) == 0:
+            return pastset
+        
+        else:
+            distance, node_edges = heappop(frontier)
+            curr = node_edges[0]
+            edges = node_edges[1]
 
+            if curr in pastset:
+                if pastset[curr][0] == distance:
+                    if pastset[curr][1] > edges:
+                      pastset[curr] = (distance, edges)
+                return sspath(pastset, frontier)
+            
+            else:
+                pastset[curr] = (distance, edges) 
+                for adjacent_node, weight in graph[curr]:
+                    heappush(frontier, (distance + weight, (adjacent_node, edges + 1)))
+                return sspath(pastset,frontier)
+    
+    frontier = []
+    heappush(frontier, (0, (source, 0)))
+    pastset = dict()
+    return sspath(pastset, frontier)
     
     
 def bfs_path(graph, source):
-    """
-    Returns:
-      a dict where each key is a vertex and the value is the parent of 
-      that vertex in the shortest path tree.
-    """
-    ###TODO
-    pass
+    
+    def bfspath(pastset, frontier,path):
+        if len(frontier)==0:
+            return path
+        else:
+            pastset=pastset|frontier 
+            frontier_update=set()
+            for node in frontier:
+                for neighbor in graph[node]:
+                    if neighbor not in pastset:
+                        path[neighbor]=node
+                        frontier_update.add(neighbor)   
+            return bfspath(pastset, frontier_update, path)
+        
+    visitedset=set()
+    startfrontier=set([source])
+    emptypath=dict()
+    return bfspath(visitedset,startfrontier,emptypath)
+    
 
 def get_sample_graph():
      return {'s': {'a', 'b'},
@@ -43,6 +77,15 @@ def get_path(parents, destination):
       The shortest path from the source node to this destination node 
       (excluding the destination node itself). See test_get_path for an example.
     """
-    ###TODO
-    pass
+    current = destination
+    final = ''
+    while True:
+        if current =='s':
+            break
+        else:
+            current = parents[current]
+            final += current
+
+
+    return final[::-1]
 
